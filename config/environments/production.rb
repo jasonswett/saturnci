bc7ad6b5302bc0778a1b6977d5d3ed6e51121fb8
Jason Swett <jason@mhcare.com>
2023-06-06 13:22:36 -0400

    Allow requests from private IPv4 addresses.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hosts << IPAddr.new("10.0.0.0/8")
+  config.hosts << IPAddr.new("172.16.0.0/12")
+  config.hosts << IPAddr.new("192.168.0.0/16")
 end
