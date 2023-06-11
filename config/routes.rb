dde49f7b27dac67f1c2009c57be26f868f5e16ef
Jason Swett <jason@benfranklinlabs.com>
2023-06-11 13:47:49 -0400

    Make button invoke spot instance.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'github_tokens/create'
+      get "github_tokens/create"
       resources :builds, only: :create
       resources :github_tokens, only: :create
     end
   end
 
   resources :projects
+  resources :builds, only: :create
   root "projects#index"
 end
