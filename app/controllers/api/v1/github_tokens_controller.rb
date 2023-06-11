45c1614cdc968890f0aed7044484bc911da45b17
Jason Swett <jason@mhcare.com>
2023-06-11 13:26:22 -0400

    Add skip_before_action :verify_authenticity_token.

--- app/controllers/api/v1/github_tokens_controller.rb
+++ app/controllers/api/v1/github_tokens_controller.rb
@@ -1,4 +1,6 @@
 class API::V1::GitHubTokensController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     render json: { token: GitHubToken.generate }
   end
