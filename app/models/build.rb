e33287aab6544604367cbbc5c887ac1e2ce48c12
Jason Swett <jason@mhcare.com>
2023-06-11 14:15:30 -0400

    Add SpotInstanceRequest.

--- app/models/build.rb
+++ app/models/build.rb
@@ -5,21 +5,10 @@ class Build < ApplicationRecord
 
   def self.start!(project)
     transaction do
-      create!(project: project)
-
-      ec2_client = Aws::EC2::Client.new(region: "us-east-2")
-      ec2_client.request_spot_instances({
-        dry_run: false,
-        spot_price: "0.05",
-        instance_count: 1,
-        type: "one-time",
-        launch_specification: {
-          image_id: "ami-024e6efaf93d85776",
-          instance_type: "t2.micro",
-          key_name: "saturn",
-          user_data: Base64.encode64("#!/bin/bash -ex\necho Hello, World!")
-        }
-      })
+      transaction do
+        build = create!(project: project)
+        SpotInstanceRequest.new(build).send!
+      end
     end
   end
 end
