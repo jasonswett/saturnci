3815f6cdf82b0fdb1bed85ab5b05a535cff04ad3
Jason Swett <jason@benfranklinlabs.com>
2023-06-14 17:41:23 -0400

    Add skip_before_token.

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,8 @@
 module API
   module V1
     class APIController < ApplicationController
+      skip_before_action :verify_authenticity_token
+
       http_basic_authenticate_with(
         name: ENV["SATURNCI_API_USERNAME"],
         password: ENV["SATURNCI_API_PASSWORD"]
