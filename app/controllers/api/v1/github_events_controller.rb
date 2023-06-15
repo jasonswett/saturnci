c6f121bde8de32c5244b2f6d25ebf32c2ec8b17f
Jason Swett <jason@mhcare.com>
2023-06-15 17:11:43 -0400

    Fix quotes.

--- app/controllers/api/v1/github_events_controller.rb
+++ app/controllers/api/v1/github_events_controller.rb
@@ -5,7 +5,7 @@ module API
         payload_raw = request.body.read
         payload = JSON.parse(payload_raw)
         Rails.logger.info "GitHub webhook payload: #{payload.inspect}"
-        render json: { message: 'success' }, status: :ok
+        render json: { message: "success" }, status: :ok
       end
     end
   end
