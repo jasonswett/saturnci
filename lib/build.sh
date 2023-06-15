f5c1272761395b878a636f563dc64ad5dfafe882
Jason Swett <jason@benfranklinlabs.com>
2023-06-15 17:02:08 -0400

    Add basic auth to all endpoints in user data script.

--- lib/build.sh
+++ lib/build.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
+
+# Function to perform API request
+function api_request() {
+    local method=$1
+    local path=$2
+    local data=$3
+
+    curl -u $SATURNCI_API_USERNAME:$SATURNCI_API_PASSWORD \
+        -X $method \
+        -H "Content-Type: application/json" \
+        -d "$data" \
+        $HOST/api/v1/$path
+}
+
 echo "Spot instance ready"
-curl -X POST -d "type=spot_instance_ready" $HOST/api/v1/builds/$BUILD_ID/build_events
+api_request "POST" "builds/$BUILD_ID/build_events" '{"type":"spot_instance_ready"}'
 
 echo "Installing Docker"
 sudo apt-get update
@@ -20,18 +35,18 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Cloning repo"
-TOKEN=$(curl -u $SATURNCI_API_USERNAME:$SATURNCI_API_PASSWORD -X POST $HOST/api/v1/github_tokens)
+TOKEN=$(api_request "POST" "github_tokens")
 USER_DIR=/home/ubuntu
 PROJECT_DIR=$USER_DIR/project
 git clone https://x-access-token:$TOKEN@github.com/jasonswett/mars $PROJECT_DIR
 cd $PROJECT_DIR
-curl -X POST -d "type=repository_cloned" $HOST/api/v1/builds/$BUILD_ID/build_events
+api_request "POST" "builds/$BUILD_ID/build_events" '{"type":"repository_cloned"}'
 
 git clone https://x-access-token:$TOKEN@github.com/jasonswett/saturnci $USER_DIR/saturnci
 cp $USER_DIR/saturnci/lib/custom_formatter.rb $PROJECT_DIR
 
 sudo docker-compose -f .saturnci/docker-compose.yml run app rails db:create
-curl -X POST -d "type=test_suite_started" $HOST/api/v1/builds/$BUILD_ID/build_events
+api_request "POST" "builds/$BUILD_ID/build_events" '{"type":"test_suite_started"}'
 
 echo "Running tests"
 RESULTS_FILENAME=$USER_DIR/build_report.json
@@ -41,7 +56,8 @@ sudo docker-compose -f .saturnci/docker-compose.yml run \
   --format CustomFormatter > $RESULTS_FILENAME
 
 echo "Test suite finished"
-curl -X POST -d "type=test_suite_finished" $HOST/api/v1/builds/$BUILD_ID/build_events
+api_request "POST" "builds/$BUILD_ID/build_events" '{"type":"test_suite_finished"}'
 
 echo "Sending report"
-curl -X POST -H "Content-Type: application/json" -d @$RESULTS_FILENAME $HOST/api/v1/builds/$BUILD_ID/build_reports
+RESULTS_CONTENT=$(cat $RESULTS_FILENAME)
+api_request "POST" "builds/$BUILD_ID/build_reports" "$RESULTS_CONTENT"
