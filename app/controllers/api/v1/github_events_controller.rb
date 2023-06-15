ecc1608aff665df3848723a645ea4af8cfb011a0
Jason Swett <jason@mhcare.com>
2023-06-15 17:08:12 -0400

    Add GitHubEventsController.

---
+++ app/controllers/api/v1/github_events_controller.rb
@@ -0,0 +1,12 @@
+module API
+  module V1
+    class GitHubEventsController < APIController
+      def create
+        payload_raw = request.body.read
+        payload = JSON.parse(payload_raw)
+        Rails.logger.info "GitHub webhook payload: #{payload.inspect}"
+        render json: { message: 'success' }, status: :ok
+      end
+    end
+  end
+end
