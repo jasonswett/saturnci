e7970bb0cb29468a0796e171689ced7860b050d7
Jason Swett <jason@benfranklinlabs.com>
2023-06-15 17:46:19 -0400

    Add debug output.

--- app/controllers/api/v1/github_events_controller.rb
+++ app/controllers/api/v1/github_events_controller.rb
@@ -7,11 +7,20 @@ module API
         Rails.logger.info "GitHub webhook payload: #{payload.inspect}"
 
         repo_full_name = params[:repository][:full_name]
+        Rails.logger.info "Finding project with full name: #{repo_full_name}"
         @project = Project.find_by!(github_repo_full_name: repo_full_name)
+
+        Rails.logger.info "Creating new build for project: #{@project.id}"
         build = Build.new(project: @project)
         build.start!
 
         head :ok
+      rescue ActiveRecord::RecordNotFound => e
+        Rails.logger.error "Project not found: #{e.message}"
+        head :not_found
+      rescue => e
+        Rails.logger.error "Error creating build: #{e.message}"
+        head :internal_server_error
       end
     end
   end
