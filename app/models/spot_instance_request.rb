2a536a8f746c824a33923dac097862d13043b32e
Jason Swett <jason@mhcare.com>
2023-06-16 17:25:38 -0400

    Revert "Add termination."
    This reverts commit 2c700f606b20c69e8860bae2764ae9dbd8c93416.

--- app/models/spot_instance_request.rb
+++ app/models/spot_instance_request.rb
@@ -16,10 +16,7 @@ class SpotInstanceRequest
         image_id: "ami-024e6efaf93d85776",
         instance_type: "t2.micro",
         key_name: "saturn",
-        user_data: user_data,
-        iam_instance_profile: {
-          name: "role_name_able_to_terminate_instances"
-        }
+        user_data: user_data
       }
     })
   end
