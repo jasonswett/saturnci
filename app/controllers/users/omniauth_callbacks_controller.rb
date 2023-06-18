ab9979f8bd146e0af2d72995271709c36685244b
Jason Swett <jason@mhcare.com>
2023-06-18 16:18:06 -0400

    Squashed commit of the following:
    commit 312a5cfee0d18480cbcaad97a492283f1922808d
    Author: Jason Swett <jason@mhcare.com>
    Date:   Sun Jun 18 16:13:33 2023 -0400
    
        Get GitHub auth fully working.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :github
 
   def github
-    raise "got here"
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
