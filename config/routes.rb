80ba68158ed20a981302ef987f401cb50c0be743
Jason Swett <jason@mhcare.com>
2023-06-18 10:53:47 -0400

    Squashed commit of the following:
    commit 1c97a210d08cbc07867094219b0f83af61c9892c
    Author: Jason Swett <jason@mhcare.com>
    Date:   Sun Jun 18 10:53:26 2023 -0400
    
        Add env vars to prod.
    
    commit 8b026cf2c51f915998ab28a1765c7ed3e7c7eb90
    Author: Jason Swett <jason@mhcare.com>
    Date:   Sun Jun 18 10:47:25 2023 -0400
    
        Get GitHub auth working.
    
    commit ca2515d7faa2a7be1a8bac89a55066434aaf009c
    Author: Jason Swett <jason@mhcare.com>
    Date:   Sun Jun 18 09:48:19 2023 -0400
    
        Add GitHub auth config.
    
    commit 78e0ac92144cf2dfb8393452496dc0a2ee4603fe
    Author: Jason Swett <jason@mhcare.com>
    Date:   Sun Jun 18 09:44:51 2023 -0400
    
        Add omniauth-github.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
   namespace :api do
     namespace :v1 do
       resources :builds, only: :create do
