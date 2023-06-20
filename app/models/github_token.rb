6f7b25bac1952e06519daae3f59192f1413c76e2
Jason Swett <jason@benfranklinlabs.com>
2023-06-19 21:04:05 -0400

    Revert "Squashed commit of the following:"
    This reverts commit 6210d5f041db8fdd9ffaf2f3cfaa676acbe0fb68.

--- app/models/github_token.rb
+++ app/models/github_token.rb
@@ -1,10 +1,22 @@
 require "jwt"
+require "octokit"
 
 class GitHubToken
+  APP_ID = 345077
   INSTALLATION_ID = 38411118
 
   def self.generate
-    client = OctokitClientFactory.create
+    private_pem = AWSSecret.read("github-private-key")
+    private_key = OpenSSL::PKey::RSA.new(private_pem)
+
+    payload = {
+      iat: Time.now.to_i, # Issued-at time
+      exp: Time.now.to_i + (10 * 60), # JWT expiration time
+      iss: APP_ID
+    }
+
+    jwt = JWT.encode(payload, private_key, "RS256")
+    client = Octokit::Client.new(bearer_token: jwt)
     installation_token = client.create_app_installation_access_token(INSTALLATION_ID)
     installation_token[:token]
   end
