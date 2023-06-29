4d4bf623d6d5665a756031fc312adc9caea67d30
Jason Swett <jason@benfranklinlabs.com>
2023-06-29 17:50:51 -0400

    Fix bug.

--- app/controllers/api/v1/github_tokens_controller.rb
+++ app/controllers/api/v1/github_tokens_controller.rb
@@ -2,7 +2,7 @@ module API
   module V1
     class GitHubTokensController < APIController
       def create
-        render plain: GitHubToken.generate(current_user.github_installation_id)
+        render plain: GitHubToken.generate(params[:github_installation_id])
       end
     end
   end
