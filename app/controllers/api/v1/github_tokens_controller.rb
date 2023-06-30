262bff30cd151fc39c31f02c43ce4c0d767f144f
Jason Swett <jason@benfranklinlabs.com>
2023-06-29 21:58:14 -0400

    Add additional logging.

--- app/controllers/api/v1/github_tokens_controller.rb
+++ app/controllers/api/v1/github_tokens_controller.rb
@@ -2,6 +2,7 @@ module API
   module V1
     class GitHubTokensController < APIController
       def create
+        Rails.logger.info "Raw request body: #{request.raw_post}"
         Rails.logger.info "GitHub token params: #{params.inspect}"
         render plain: GitHubToken.generate(params[:github_installation_id])
       end
