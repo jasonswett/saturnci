54d8fa6dda12c13df9111b579b881c42af6883e8
Jason Swett <jason@benfranklinlabs.com>
2023-06-30 08:46:35 -0400

    Add commit to builds.

--- app/controllers/api/v1/github_events_controller.rb
+++ app/controllers/api/v1/github_events_controller.rb
@@ -32,12 +32,11 @@ module API
       def handle_push_event(payload)
         repo_full_name = params[:repository][:full_name]
         @project = Project.find_by!(github_repo_full_name: repo_full_name)
-        build = Build.new(project: @project)
 
         ref_path = payload["ref"]
-        branch_name = ref_path.split("/").last
-        build.branch_name = branch_name
-        Rails.logger.info "Branch name: #{build.branch_name}"
+        build = Build.new(project: @project)
+        build.branch_name = ref_path.split("/").last
+        build.commit = payload["after"]
         build.start!
       end
     end
