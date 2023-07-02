7953b635890aa028ad7d79c7b20e0afc049c1279
Jason Swett <jason@benfranklinlabs.com>
2023-07-02 10:15:33 -0400

    Get rebuild button working.

--- app/controllers/rebuilds_controller.rb
+++ app/controllers/rebuilds_controller.rb
@@ -1,7 +1,9 @@
 class RebuildsController < ApplicationController
   def create
     original_build = Build.find(params[:build_id])
-    build = Build.new(project: original_build.project)
+    build = original_build.dup
     build.start!
+
+    redirect_to build.project
   end
 end
