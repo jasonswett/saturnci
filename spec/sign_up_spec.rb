3c1f9b0afd0200e69957c57037b0d72896a56918
Jason Swett <jason@benfranklinlabs.com>
2023-07-05 21:53:49 -0400

    Add test for sign-up.

---
+++ spec/sign_up_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Sign up with Github", type: :system do
+  context "success" do
+    it "redirects to projects page" do
+      OmniAuth.config.test_mode = true
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+        provider: "github",
+        uid: "123545",
+        info: {
+          email: "test@example.com",
+          name: "John Smith"
+        },
+        credentials: {
+          token: "mock_token_123"
+        }
+      )
+
+      visit new_user_registration_path
+      click_on "Sign up with GitHub"
+      
+      expect(page).to have_content("Add project")
+    end
+  end
+end
