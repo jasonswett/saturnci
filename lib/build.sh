381f53b2f2f1c7ab30781b3831c5cca19cba92c4
Jason Swett <jason@benfranklinlabs.com>
2024-01-05 13:54:23 -0500

    Add dividers.

--- lib/build.sh
+++ lib/build.sh
@@ -13,8 +13,12 @@ function api_request() {
         $HOST/api/v1/$path
 }
 
+#--------------------------------------------------------------------------------
+
 echo "Spot instance ready"
 api_request "POST" "builds/$BUILD_ID/build_events" '{"type":"spot_instance_ready"}'
+
+#--------------------------------------------------------------------------------
 
 echo "Installing Docker"
 sudo apt-get update
@@ -22,6 +26,8 @@ sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+#--------------------------------------------------------------------------------
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
@@ -34,6 +40,8 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plug
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+#--------------------------------------------------------------------------------
+
 echo "Cloning user repo"
 TOKEN=$(api_request "POST" "github_tokens" "{\"github_installation_id\":\"$GITHUB_INSTALLATION_ID\"}")
 USER_DIR=/home/ubuntu
@@ -42,12 +50,16 @@ git clone https://x-access-token:$TOKEN@github.com/$GITHUB_REPO_FULL_NAME $PROJE
 cd $PROJECT_DIR
 api_request "POST" "builds/$BUILD_ID/build_events" '{"type":"repository_cloned"}'
 
+#--------------------------------------------------------------------------------
+
 echo "Cloning Saturn"
 git clone https://x-access-token:$TOKEN@github.com/jasonswett/saturnci $USER_DIR/saturnci
 cp $USER_DIR/saturnci/lib/custom_formatter.rb $PROJECT_DIR
 
 sudo docker-compose -f .saturnci/docker-compose.yml run app rails db:create
 api_request "POST" "builds/$BUILD_ID/build_events" '{"type":"test_suite_started"}'
+
+#--------------------------------------------------------------------------------
 
 echo "Running tests"
 RESULTS_FILENAME=$USER_DIR/build_report.json
@@ -56,12 +68,18 @@ sudo docker-compose -f .saturnci/docker-compose.yml run \
   --require ./custom_formatter.rb \
   --format CustomFormatter > $RESULTS_FILENAME
 
+#--------------------------------------------------------------------------------
+
 echo "Test suite finished"
 api_request "POST" "builds/$BUILD_ID/build_events" '{"type":"test_suite_finished"}'
+
+#--------------------------------------------------------------------------------
 
 echo "Sending report"
 RESULTS_CONTENT=$(cat $RESULTS_FILENAME)
 api_request "POST" "builds/$BUILD_ID/build_reports" "$RESULTS_CONTENT"
+
+#--------------------------------------------------------------------------------
 
 echo "Sending logs"
 curl -u $SATURNCI_API_USERNAME:$SATURNCI_API_PASSWORD \
