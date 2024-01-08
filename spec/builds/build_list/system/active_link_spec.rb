272d579df6312a704659cc913c61cd453287003f
Jason Swett <jason@benfranklinlabs.com>
2024-01-08 07:03:03 -0500

    Make it so correct link gets activated upon page load.

--- spec/builds/build_list/system/active_link_spec.rb
+++ spec/builds/build_list/system/active_link_spec.rb
@@ -8,22 +8,27 @@ describe "Active link", type: :system do
   before do
     user = create(:user)
     login_as(user, scope: :user)
+    visit project_build_path(project, build_1)
   end
 
   context "link clicked" do
     it "sets that build to active" do
-      visit project_build_path(project, build_1)
       click_on "build_link_#{build_2.id}"
       expect(page.find("#build_link_#{build_2.id}")[:class].split).to include("active")
     end
 
     it "sets other builds to inactive" do
-      visit project_build_path(project, build_1)
       click_on "build_link_#{build_2.id}"
       expect(page.find("#build_link_#{build_2.id}")[:class].split).to include("active")
 
       click_on "build_link_#{build_1.id}"
       expect(page.find("#build_link_#{build_2.id}")[:class].split).not_to include("active")
+    end
+  end
+
+  context "page load" do
+    it "sets the first build link to active" do
+      expect(page.find("#build_link_#{build_1.id}")[:class].split).to include("active")
     end
   end
 end
