30e0cb5abe40fd50aeb02dd498f58c85ea5db31c
Jason Swett <jason@benfranklinlabs.com>
2024-01-10 11:13:59 -0500

    Fix redirect.

--- spec/builds/system/delete_build_spec.rb
+++ spec/builds/system/delete_build_spec.rb
@@ -8,10 +8,21 @@ describe "Delete build", type: :system do
     login_as(user, scope: :user)
   end
 
-  context "main branch" do
-    it "only shows builds from the main branch" do
+  context "branch is only branch" do
+    it "removes the build" do
       visit project_build_path(id: build.id, project_id: build.project.id)
       click_on "Delete"
+      expect(page).not_to have_content(build.commit_hash)
+    end
+  end
+
+  context "branch is not the only branch" do
+    it "removes the build" do
+      create(:build, project: build.project)
+
+      visit project_build_path(id: build.id, project_id: build.project.id)
+      click_on "Delete"
+      binding.pry
       expect(page).not_to have_content(build.commit_hash)
     end
   end
