0d9927294fac9ca6b9d5dd033dde08a65b644a64
Jason Swett <jason@benfranklinlabs.com>
2024-01-10 17:25:50 -0500

    Misc.

--- spec/builds/system/delete_build_spec.rb
+++ spec/builds/system/delete_build_spec.rb
@@ -4,6 +4,8 @@ describe "Delete build", type: :system do
   let!(:build) { create(:build) }
 
   before do
+    stub_request(:delete, "https://api.digitalocean.com/v2/droplets").to_return(status: 200)
+
     user = create(:user)
     login_as(user, scope: :user)
   end
@@ -22,7 +24,6 @@ describe "Delete build", type: :system do
 
       visit project_build_path(id: build.id, project_id: build.project.id)
       click_on "Delete"
-      binding.pry
       expect(page).not_to have_content(build.commit_hash)
     end
   end
