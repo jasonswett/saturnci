830175cbcbd130a781b1dd465e5e55a604da8ab7
Jason Swett <jason@benfranklinlabs.com>
2024-01-12 08:08:19 -0500

    Remove destroy action.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -31,15 +31,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  def destroy
-    @project.destroy
-
-    respond_to do |format|
-      format.html { redirect_to projects_url, notice: "Project was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
