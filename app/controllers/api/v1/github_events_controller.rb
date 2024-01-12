b02e58f83cef4860be88545e651620f299c462c2
Jason Swett <jason@benfranklinlabs.com>
2024-01-11 19:44:28 -0500

    Make it possible to install Saturn as an organization.

--- app/controllers/api/v1/github_events_controller.rb
+++ app/controllers/api/v1/github_events_controller.rb
@@ -23,8 +23,12 @@ module API
 
       def handle_installation_event(payload)
         github_installation_id = payload["installation"]["id"]
-        github_account_id = payload["installation"]["account"]["id"]
-        #github_account_id = payload["sender"]["id"]
+
+        if payload["installation"]["account"]["type"] == "Organization"
+          github_account_id = payload["sender"]["id"]
+        else
+          github_account_id = payload["installation"]["account"]["id"]
+        end
 
         user = User.find_by!(uid: github_account_id, provider: "github")
         user.saturn_installations.create!(github_installation_id: github_installation_id)
