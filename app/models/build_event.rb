dc8fc3120b628563dae07b12d2a8ee5c3704888d
Jason Swett <jason@benfranklinlabs.com>
2024-01-14 08:49:07 -0500

    Squashed commit of the following:
    commit 154ddf1a4180a4dfebdcca8c6003a7277c681cb3
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Sun Jan 14 08:35:02 2024 -0500
    
        Put build in separate step.

--- app/models/build_event.rb
+++ app/models/build_event.rb
@@ -9,6 +9,8 @@ class BuildEvent < ApplicationRecord
     test_suite_started: 3,
     test_suite_finished: 4,
     pre_script_started: 5,
-    pre_script_finished: 6
+    pre_script_finished: 6,
+    image_build_started: 7,
+    image_build_finished: 8
   }
 end
