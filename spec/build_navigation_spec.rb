5428cb67da9167f60d179e178faa02bbed993162
Jason Swett <jason@benfranklinlabs.com>
2024-01-14 09:40:09 -0500

    Fix test.

--- spec/build_navigation_spec.rb
+++ spec/build_navigation_spec.rb
@@ -1,34 +1,36 @@
 require "rails_helper"
 
 describe "Build navigation", type: :system do
-  let!(:build) { create(:build) }
-  let!(:build_heading) { "Commit #{build.commit_hash[0..7]}" }
+  let!(:first_build) { create(:build) }
+  let!(:second_build) { create(:build, project: first_build.project) }
 
   before do
-    login_as(build.project.user, scope: :user)
+    login_as(first_build.project.user, scope: :user)
   end
 
-  describe "clicking build link" do
+  describe "clicking on second build after having visited first build" do
     before do
-      visit project_path(build.project)
-      click_on "build_link_#{build.id}"
+      visit project_build_path(first_build.project, first_build)
+      click_on "build_link_#{second_build.id}"
+      expect(page).to have_content(pane_heading(second_build)) # to prevent race condition
     end
 
-    it "keeps the left pane (build list)" do
-      expect(page).to have_content(build_heading) # to prevent race condition
-      expect(page).to have_content(build.project.name)
+    it "retains the first build in the build list" do
+      expect(page).to have_content(first_build.project.name)
     end
 
-    it "after refresh, keeps right pane (build detail)" do
-      expect(page).to have_content(build_heading)
+    it "after refresh, keeps second build in detail pane" do
       visit current_url
-      expect(page).to have_content(build_heading)
+      expect(page).to have_content(pane_heading(second_build))
     end
 
     it "after refresh, keeps left pane (build list)" do
-      expect(page).to have_content(build_heading)
       visit current_url
-      expect(page).to have_content(build.project.name)
+      expect(page).to have_content(first_build.project.name)
     end
+  end
+
+  def pane_heading(build)
+    "Commit #{build.commit_hash[0..7]}"
   end
 end
