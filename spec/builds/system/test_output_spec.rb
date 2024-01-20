cb2c6b50b7f7d301ccc03fd043869e74b3e01ffe
Jason Swett <jason@benfranklinlabs.com>
2024-01-20 12:26:42 -0500

    Fix test.

--- spec/builds/system/test_output_spec.rb
+++ spec/builds/system/test_output_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-describe "Test report", type: :system do
-  let!(:build) { create(:build, report: "this is the test report") }
+describe "Test output", type: :system do
+  let!(:build) { create(:build, test_output: "this is the test output") }
 
   before do
     login_as(build.project.user, scope: :user)
@@ -9,6 +9,6 @@ describe "Test report", type: :system do
 
   it "is the default" do
     visit project_build_path(build.project, build)
-    expect(page).to have_content("this is the test report")
+    expect(page).to have_content("this is the test output")
   end
 end
