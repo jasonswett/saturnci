5c6ac52dd931ea2caa9c7a950de7fb387cf7aca2
Jason Swett <jason@benfranklinlabs.com>
2024-01-21 13:40:02 -0500

    Fix rebuild.

--- app/models/rebuild.rb
+++ app/models/rebuild.rb
@@ -1,7 +1,11 @@
 class Rebuild
   def self.create!(original_build)
-    original_build.dup.tap do |build|
-      build.report = nil
-    end
+    Build.create!(
+      project: original_build.project,
+      branch_name: original_build.branch_name,
+      commit_hash: original_build.commit_hash,
+      commit_message: original_build.commit_message,
+      author_name: original_build.author_name
+    )
   end
 end
