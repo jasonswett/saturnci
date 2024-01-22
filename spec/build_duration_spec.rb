773161c985ccfb828e9ec9b099aae8b00ee666d1
Jason Swett <jason@benfranklinlabs.com>
2024-01-22 08:24:14 -0500

    Fix duration issue.

--- spec/build_duration_spec.rb
+++ spec/build_duration_spec.rb
@@ -2,19 +2,18 @@ require "rails_helper"
 
 describe "Duration", type: :system do
   it "displays the build duration" do
-    project = create(:project)
-    build = create(:build, project: project)
+    job = create(:job)
 
     create(
-      :build_event,
+      :job_event,
       type: :test_suite_finished,
-      build: build,
-      created_at: build.created_at + ((5 * 60) + 10).seconds
+      job: job,
+      created_at: job.created_at + ((5 * 60) + 10).seconds
     )
 
     user = create(:user)
     login_as(user, scope: :user)
-    visit project_path(project)
+    visit project_path(job.build.project)
 
     expect(page).to have_content("5m 10s")
   end
--- spec/models/build_spec.rb
+++ spec/models/build_spec.rb
@@ -1,6 +1,48 @@
 require "rails_helper"
 
 RSpec.describe Build, type: :model do
+  describe "#duration" do
+    let!(:build) { create(:build) }
+
+    context "no jobs" do
+      it "is nil" do
+        expect(build.duration).to be nil
+      end
+    end
+
+    context "one job finished, the other not finished yet" do
+      before do
+        finished_job = create(:job, build: build, order_index: 1)
+        allow(finished_job).to receive(:duration).and_return(5)
+
+        unfinished_job = create(:job, build: build, order_index: 2)
+        allow(finished_job).to receive(:duration).and_return(nil)
+
+        allow(build).to receive(:jobs).and_return([finished_job, unfinished_job])
+      end
+
+      it "is nil" do
+        expect(build.duration).to be nil
+      end
+    end
+
+    context "two finished jobs" do
+      before do
+        shorter_job = create(:job, build: build, order_index: 1)
+        allow(shorter_job).to receive(:duration).and_return(5)
+
+        longer_job = create(:job, build: build, order_index: 2)
+        allow(longer_job).to receive(:duration).and_return(7)
+
+        allow(build).to receive(:jobs).and_return([shorter_job, longer_job])
+      end
+
+      it "returns the longer of the two jobs" do
+        expect(build.duration).to eq(7)
+      end
+    end
+  end
+
   describe "#start!" do
     let!(:job) { create(:job) }
     let!(:build) { job.build }
