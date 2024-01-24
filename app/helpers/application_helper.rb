9b90255698973da81270bc333f3449679e75434f
Jason Swett <jason@benfranklinlabs.com>
2024-01-24 11:29:22 -0500

    Improve test output appearance.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def abbreviated_hash(hash)
     hash[0..7]
   end
+
+  def terminal_tag
+    content = capture { yield }
+    compressed_content = content.gsub(/\n\s+/, "").html_safe
+    content_tag(:pre, compressed_content, class: "terminal")
+  end
 end
