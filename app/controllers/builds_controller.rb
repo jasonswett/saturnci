16ae822046426a3271f1b9f21bd288f2ab3bab9c
Jason Swett <jason@benfranklinlabs.com>
2024-01-25 20:04:54 -0500

    Refactoring.

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -10,7 +10,10 @@ class BuildsController < ApplicationController
   end
 
   def show
-    render_show(params[:id], partial: params[:partial] || DEFAULT_PARTIAL)
+    @build = Build.find(params[:id])
+    @project = @build.project
+    @build_list = BuildList.new(@build, branch_name: params[:branch_name])
+    @partial = params[:partial] || DEFAULT_PARTIAL
   end
 
   def destroy
@@ -28,15 +31,5 @@ class BuildsController < ApplicationController
 
     build.destroy!
     redirect_to project_path(build.project)
-  end
-
-  private
-
-  def render_show(build_id, partial:)
-    @build = Build.find(build_id)
-    @project = @build.project
-    @build_list = BuildList.new(@build, branch_name: params[:branch_name])
-    @partial = partial
-    render "show"
   end
 end
