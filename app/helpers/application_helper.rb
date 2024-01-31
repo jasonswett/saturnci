1a3ec8ba7f8a8d2906762d778f2032a24a9c7842
Jason Swett <jason@benfranklinlabs.com>
2024-01-31 10:31:30 -0500

    Refactor build nav.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,10 +10,4 @@ module ApplicationHelper
     compressed_content = content.gsub(/\n\s+/, "").html_safe
     content_tag(:pre, compressed_content, class: "terminal")
   end
-
-  def build_navigation_link_to(text, slug, build, partial)
-    link_to text, build_detail_content_project_build_path(build.project, build, slug),
-      class: partial == slug ? "active" : "",
-      data: { turbo_frame: "build_details" }
-  end
 end
---
+++ app/helpers/build_navigation_helper.rb
@@ -0,0 +1,23 @@
+class BuildNavigationItem
+  def initialize(view_context, build, partial)
+    @view_context = view_context
+    @build = build
+    @partial = partial
+  end
+
+  def item(text, slug)
+    @view_context.link_to(
+      text,
+      @view_context.build_detail_content_project_build_path(@build.project, @build, slug),
+      class: @partial == slug ? "active" : "",
+      data: { turbo_frame: "build_details" }
+    )
+  end
+end
+
+module BuildNavigationHelper
+  def build_navigation(build, partial, &block)
+    build_navigation = BuildNavigationItem.new(self, build, partial)
+    block.call(build_navigation)
+  end
+end
