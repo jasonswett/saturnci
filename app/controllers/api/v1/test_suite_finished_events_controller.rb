8a3156f8a9834871022405973cb2688cbb9f0016
Jason Swett <jason@benfranklinlabs.com>
2024-02-04 10:17:15 -0500

    Add transaction.

--- app/controllers/api/v1/test_suite_finished_events_controller.rb
+++ app/controllers/api/v1/test_suite_finished_events_controller.rb
@@ -3,10 +3,13 @@ module API
     class TestSuiteFinishedEventsController < APIController
       def create
         job = Job.find(params[:job_id])
-        job.job_events.create!(type: "test_suite_finished")
 
-        if job.build.jobs.all?(&:finished?)
-          job.build.update!(finished_at: Time.zone.now)
+        ActiveRecord::Base.transaction do
+          job.job_events.create!(type: "test_suite_finished")
+
+          if job.build.jobs.all?(&:finished?)
+            job.build.update!(finished_at: Time.zone.now)
+          end
         end
 
         head :ok
