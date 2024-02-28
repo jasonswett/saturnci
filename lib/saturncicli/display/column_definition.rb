a9210f514153af2bb124370b7810fab9e8ad503b
Jason Swett <jason@benfranklinlabs.com>
2024-02-28 18:28:01 -0500

    Refactoring.

--- lib/saturncicli/display/column_definition.rb
+++ lib/saturncicli/display/column_definition.rb
@@ -2,9 +2,16 @@ module SaturnCICLI
   module Display
     class ColumnDefinition
       def self.find(resource_name)
+        column_definition_class(resource_name).new
+      end
+
+      def self.column_definition_class(resource_name)
         require_relative "#{resource_name}_table_column_definitions"
-        class_name = "SaturnCICLI::Display::#{resource_name.to_s.capitalize}TableColumnDefinitions"
-        Object.const_get(class_name).new
+        Object.const_get(class_name(resource_name))
+      end
+
+      def self.class_name(resource_name)
+        "SaturnCICLI::Display::#{resource_name.to_s.capitalize}TableColumnDefinitions"
       end
     end
   end
