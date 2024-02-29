b6b096b092657bcd3a885a672d3c03f4c21dfc04
Jason Swett <jason@benfranklinlabs.com>
2024-02-29 11:13:23 -0500

    Swap order.

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -2,8 +2,8 @@ module API
   module V1
     class JobsController < APIController
       def index
-        render json: Job.order("created_at desc")
-          .running
+        render json: Job.running
+          .order("created_at desc")
           .as_json(methods: :status)
       end
     end
