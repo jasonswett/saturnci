d6fab0187dc50c72231756869787324ab58a4ec0
Jason Swett <jason@benfranklinlabs.com>
2024-02-29 14:06:36 -0500

    Fix test.

--- spec/saturncicli/client_spec.rb
+++ spec/saturncicli/client_spec.rb
@@ -7,26 +7,31 @@ describe "client" do
     before do
       AuthenticationHelper.stub_authentication_request
 
+
       body = [
         {
+          "id" => "cdbe84c7",
           "build_id" => "3cbe1b26",
           "created_at" => "2024-02-28 15:38:06",
           "status" => "Passed",
           "build_commit_message" => "Did stuff",
         },
         {
+          "id" => "6882b373",
           "build_id" => "56d2a863",
           "created_at" => "2024-02-28 15:36:18",
           "status" => "Passed",
           "build_commit_message" => "Did other stuff",
         },
         {
+          "id" => "4c304b55",
           "build_id" => "7c8dd048",
           "created_at" => "2024-02-28 01:57:05",
           "status" => "Passed",
           "build_commit_message" => "Did yet other stuff",
         },
         {
+          "id" => "4cdfb661",
           "build_id" => "65a7e250",
           "created_at" => "2024-02-28 01:52:07",
           "status" => "Passed",
@@ -46,11 +51,11 @@ describe "client" do
 
     it "shows jobs" do
       expected_output = <<~OUTPUT
-      Created              Build status  Build ID  Build commit message
-      2024-02-28 15:38:06  Passed        3cbe1b26  Did stuff
-      2024-02-28 15:36:18  Passed        56d2a863  Did other stuff
-      2024-02-28 01:57:05  Passed        7c8dd048  Did yet other stuff
-      2024-02-28 01:52:07  Passed        65a7e250  Did similar but different stuff
+      ID        Created              Build status  Build ID  Build commit message
+      cdbe84c7  2024-02-28 15:38:06  Passed        3cbe1b26  Did stuff
+      6882b373  2024-02-28 15:36:18  Passed        56d2a863  Did other stuff
+      4c304b55  2024-02-28 01:57:05  Passed        7c8dd048  Did yet other stuff
+      4cdfb661  2024-02-28 01:52:07  Passed        65a7e250  Did similar but different stuff
       OUTPUT
 
       expect {
