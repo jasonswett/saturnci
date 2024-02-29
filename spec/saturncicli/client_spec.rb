144e504532aab50a9725a7c5920c215ff6d11b08
Jason Swett <jason@benfranklinlabs.com>
2024-02-29 11:37:13 -0500

    Fix test.

--- spec/saturncicli/client_spec.rb
+++ spec/saturncicli/client_spec.rb
@@ -10,19 +10,27 @@ describe "client" do
       body = [
         {
           "build_id" => "3cbe1b26",
-          "created_at" => "2024-02-28 15:38:06"
+          "created_at" => "2024-02-28 15:38:06",
+          "status" => "Passed",
+          "build_commit_message" => "Did stuff",
         },
         {
           "build_id" => "56d2a863",
-          "created_at" => "2024-02-28 15:36:18"
+          "created_at" => "2024-02-28 15:36:18",
+          "status" => "Passed",
+          "build_commit_message" => "Did other stuff",
         },
         {
           "build_id" => "7c8dd048",
-          "created_at" => "2024-02-28 01:57:05"
+          "created_at" => "2024-02-28 01:57:05",
+          "status" => "Passed",
+          "build_commit_message" => "Did yet other stuff",
         },
         {
           "build_id" => "65a7e250",
-          "created_at" => "2024-02-28 01:52:07"
+          "created_at" => "2024-02-28 01:52:07",
+          "status" => "Passed",
+          "build_commit_message" => "Did similar but different stuff",
         }
       ].to_json
 
@@ -38,11 +46,11 @@ describe "client" do
 
     it "shows jobs" do
       expected_output = <<~OUTPUT
-      Build ID  Created
-      3cbe1b26  2024-02-28 15:38:06
-      56d2a863  2024-02-28 15:36:18
-      7c8dd048  2024-02-28 01:57:05
-      65a7e250  2024-02-28 01:52:07
+      Created              Build status  Build ID  Build commit message
+      2024-02-28 15:38:06  Passed        3cbe1b26  Did stuff
+      2024-02-28 15:36:18  Passed        56d2a863  Did other stuff
+      2024-02-28 01:57:05  Passed        7c8dd048  Did yet other stuff
+      2024-02-28 01:52:07  Passed        65a7e250  Did similar but different stuff
       OUTPUT
 
       expect {
