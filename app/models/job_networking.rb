aced94162d08ad7ca74f13b9e92c569265de4a17
Jason Swett <jason@benfranklinlabs.com>
2024-03-04 11:22:48 -0500

    Refactoring.

--- app/models/job_networking.rb
+++ app/models/job_networking.rb
@@ -1,7 +1,19 @@
 module JobNetworking
   def ip_address
-    client = DropletKitClientFactory.client
-    droplet = client.droplets.find(id: job_machine_id)
-    ip_address = droplet.networks.v4.find { |network| network.type == 'public' }.ip_address
+    network(droplet).ip_address
+  end
+
+  private
+
+  def network(droplet)
+    droplet.networks.v4.find { |network| network.type == 'public' }
+  end
+
+  def droplet
+    client.droplets.find(id: job_machine_id)
+  end
+
+  def client
+    @client ||= DropletKitClientFactory.client
   end
 end
--- lib/droplet_kit_client_factory.rb
+++ lib/droplet_kit_client_factory.rb
@@ -1,5 +1,5 @@
 class DropletKitClientFactory
   def self.client
-    @client ||= DropletKit::Client.new(access_token: ENV['DIGITALOCEAN_ACCESS_TOKEN'])
+    DropletKit::Client.new(access_token: ENV['DIGITALOCEAN_ACCESS_TOKEN'])
   end
 end
