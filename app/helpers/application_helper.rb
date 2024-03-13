7cfaf9ffef98c726db8e42a5842ef66a54f47671
Jason Swett <jason@benfranklinlabs.com>
2024-03-13 18:43:11 -0400

    Add ascii_job_heading helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
     compressed_content = content.gsub(/\n\s+/, "").html_safe
     content_tag(:pre, compressed_content, class: "terminal")
   end
+
+  def ascii_job_heading(job_order_index)
+    line = "-" * 40
+    content_tag(:div) do
+      concat("#{line}<br>".html_safe)
+      concat("Job #{job_order_index}<br>".html_safe)
+      concat("#{line}<br><br>".html_safe)
+    end
+  end
 end
