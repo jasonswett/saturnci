3012c82d3584e6f8276e9ec922aa4893edb8fd1f
Jason Swett <jason@benfranklinlabs.com>
2024-04-28 22:06:45 -0400

    Experimentally remove mirror.

--- lib/job.sh
+++ lib/job.sh
@@ -35,16 +35,6 @@ api_request "POST" "jobs/$JOB_ID/job_events" '{"type":"job_machine_ready"}'
 
 #--------------------------------------------------------------------------------
 
-echo "Configuring Docker to use the registry cache"
-sudo mkdir -p /etc/docker
-echo '{
-  "registry-mirrors": ["https://registrycache.saturnci.com:5000"]
-}' | sudo tee /etc/docker/daemon.json
-
-sudo systemctl restart docker
-
-#--------------------------------------------------------------------------------
-
 echo "Cloning user repo"
 TOKEN=$(api_request "POST" "github_tokens" "{\"github_installation_id\":\"$GITHUB_INSTALLATION_ID\"}")
 git clone https://x-access-token:$TOKEN@github.com/$GITHUB_REPO_FULL_NAME $PROJECT_DIR
