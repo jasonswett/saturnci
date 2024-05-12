27911a9e4d7f336e033a2a9fe2926a57a96520db
Jason Swett <jason@benfranklinlabs.com>
2024-05-12 09:11:59 -0400

    Add debug output.

--- app/controllers/api/v1/system_logs_controller.rb
+++ app/controllers/api/v1/system_logs_controller.rb
@@ -2,9 +2,16 @@ module API
   module V1
     class SystemLogsController < APIController
       def create
+        # The system logs aren't showing up.
+        # Is the problem that the job isn't sending the logs properly
+        # or that this controller isn't saving them properly?
         job = Job.find(params[:job_id])
         request.body.rewind
         log_chunk = request.body.read
+        puts "-" * 80
+        puts "log chunk:"
+        puts log_chunk
+        puts "-" * 80
 
         # Append the new chunk of logs to the existing logs
         job.system_logs = (job.system_logs || "") + log_chunk
