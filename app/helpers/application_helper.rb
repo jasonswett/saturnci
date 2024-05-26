44a5ffbc73f45e52904fd3400f5e83189fe81455
Jason Swett <jason@benfranklinlabs.com>
2024-05-26 09:33:25 -0400

    Take away redundant ASCII job heading.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,23 +12,12 @@ module ApplicationHelper
     content_tag(:div, terminal_content, class: "job-info-container")
   end
 
-  def ascii_job_heading(job_order_index)
-    line = "-" * 40
-    content_tag(:div) do
-      concat("#{line}<br>".html_safe)
-      concat("Job #{job_order_index}<br>".html_safe)
-      concat("#{line}<br><br>".html_safe)
-    end
-  end
-
   def job_container(current_tab_name, job, &block)
-    content = ascii_job_heading(job.order_index)
-
     job_info = capture { yield }
     if job_info.present?
-      content + content_tag(:div, id: dom_id(job, current_tab_name), &block)
+      content_tag(:div, id: dom_id(job, current_tab_name), &block)
     else
-      content + content_tag(:div, "Nothing here yet")
+      content_tag(:div, "Nothing here yet")
     end
   end
 end
