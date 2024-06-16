77c82b6d291c414b269a2ff0b842f1ec5bf8b2f3
Jason Swett <jason@benfranklinlabs.com>
2024-06-16 09:54:45 -0400

    DRY up SystemLogsController.

--- app/controllers/api/v1/system_logs_controller.rb
+++ app/controllers/api/v1/system_logs_controller.rb
@@ -1,20 +1,16 @@
 module API
   module V1
     class SystemLogsController < APIController
-      TAB_NAME = "system_logs"
-
       def create
         job = Job.find(params[:job_id])
         request.body.rewind
         log_chunk = request.body.read
         job.update!(system_logs: job.system_logs.to_s + log_chunk)
 
-        Turbo::StreamsChannel.broadcast_update_to(
-          "job_#{job.id}_#{TAB_NAME}",
-          target: "build_details_content_#{TAB_NAME}",
-          partial: "jobs/#{TAB_NAME}",
-          locals: { job: job, current_tab_name: "#{TAB_NAME}" }
-        )
+        Streaming::JobOutputStream.new(
+          job: job,
+          tab_name: "system_logs"
+        ).broadcast
 
         head :ok
       end
