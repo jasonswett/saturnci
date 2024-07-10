d0d0dc4a19163338f79e847e33150e49cc974522
Jason Swett <jason@benfranklinlabs.com>
2024-07-10 19:10:50 -0400

    Add build created at time.

--- app/javascript/controllers/elapsed_build_time_controller.js
+++ app/javascript/controllers/elapsed_build_time_controller.js
@@ -2,11 +2,14 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["value"];
+  static values = { buildCreatedAtDatetime: String };
 
   connect() {
     if (!this.hasValueTarget) {
       return;
     }
+
+    console.log(this.buildCreatedAtDatetimeValue);
 
     setInterval(() => {
       this.valueTarget.textContent = this.elapsedTime();
