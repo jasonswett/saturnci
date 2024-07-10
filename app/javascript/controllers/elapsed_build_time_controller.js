ab4405704f5e51b747761751165620a0a170422f
Jason Swett <jason@benfranklinlabs.com>
2024-07-10 18:51:08 -0400

    Fix JavaScript error.

--- app/javascript/controllers/elapsed_build_time_controller.js
+++ app/javascript/controllers/elapsed_build_time_controller.js
@@ -4,6 +4,10 @@ export default class extends Controller {
   static targets = ["value"];
 
   connect() {
+    if (!this.hasValueTarget) {
+      return;
+    }
+
     this.valueTarget.textContent = "1234";
   }
 }
