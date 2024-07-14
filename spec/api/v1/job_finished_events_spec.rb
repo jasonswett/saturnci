8ddcf9e18fd6b9d9f5431fd534cde47c31d3255e
Jason Swett <jason@benfranklinlabs.com>
2024-07-14 12:00:28 -0400

    Delete obsolete test.

--- spec/api/v1/job_finished_events_spec.rb
+++ spec/api/v1/job_finished_events_spec.rb
@@ -22,20 +22,5 @@ RSpec.describe "job finished events", type: :request do
       expect(response).to have_http_status(200)
       expect(response.body).to be_empty
     end
-
-    context "not all the jobs have finished" do
-      before do
-        create(:job, build: job.build, order_index: 2)
-      end
-
-      it "sets the build's finished_at value" do
-        post(
-          api_v1_job_job_finished_events_path(job),
-          headers: api_authorization_headers
-        )
-
-        expect(job.build.reload.finished_at).to be nil
-      end
-    end
   end
 end
