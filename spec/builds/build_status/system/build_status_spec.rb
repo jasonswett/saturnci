9a48e103b4ff5fa467ceea57f413505f884621de
Jason Swett <jason@benfranklinlabs.com>
2024-07-21 09:17:50 -0400

    Improve test organization.

--- spec/builds/build_status/system/build_status_spec.rb
+++ spec/builds/build_status/system/build_status_spec.rb
@@ -23,40 +23,42 @@ describe "Build status", type: :system do
     end
   end
 
-  context "running build" do
-    it "does not show the elapsed build time" do
-      visit project_build_path(job.build.project, job.build)
-      expect(page).to have_selector("[data-elapsed-build-time-target='value']")
-    end
-  end
-
-  context "finished build" do
-    it "shows the elapsed build time" do
-      job.update!(test_report: "passed")
-      visit project_build_path(job.build.project, job.build)
-      expect(page).not_to have_selector("[data-elapsed-build-time-target='value']")
-    end
-  end
-
-  context "when the build goes from running to finished" do
-    before do
-      allow_any_instance_of(Build).to receive(:duration_formatted).and_return("3m 40s")
-      visit project_build_path(job.build.project, job.build)
+  describe "elapsed time" do
+    context "running build" do
+      it "does not show the elapsed build time" do
+        visit project_build_path(job.build.project, job.build)
+        expect(page).to have_selector("[data-elapsed-build-time-target='value']")
+      end
     end
 
-    it "goes from counting to not counting" do
-      # We expect the page not to have "3m 40s" because,
-      # before the build finishes, the counter will be counting
-      expect(page).not_to have_content("3m 40s")
+    context "finished build" do
+      it "shows the elapsed build time" do
+        job.update!(test_report: "passed")
+        visit project_build_path(job.build.project, job.build)
+        expect(page).not_to have_selector("[data-elapsed-build-time-target='value']")
+      end
+    end
 
-      http_request(
-        api_authorization_headers: api_authorization_headers,
-        path: api_v1_job_job_finished_events_path(job)
-      )
+    context "when the build goes from running to finished" do
+      before do
+        allow_any_instance_of(Build).to receive(:duration_formatted).and_return("3m 40s")
+        visit project_build_path(job.build.project, job.build)
+      end
 
-      # After the build finishes, the counter will have
-      # stopped counting and we just see the static duration
-      expect(page).to have_content("3m 40s")
+      it "changes the elapsed time from counting to not counting" do
+        # We expect the page not to have "3m 40s" because,
+        # before the build finishes, the counter will be counting
+        expect(page).not_to have_content("3m 40s")
+
+        http_request(
+          api_authorization_headers: api_authorization_headers,
+          path: api_v1_job_job_finished_events_path(job)
+        )
+
+        # After the build finishes, the counter will have
+        # stopped counting and we just see the static duration
+        expect(page).to have_content("3m 40s")
+      end
     end
   end
 end
