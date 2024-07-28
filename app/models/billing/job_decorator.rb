74cd71ff3b5b26b4cb5c6a3a9bcc7c2ee80acf10
Jason Swett <jason@benfranklinlabs.com>
2024-07-28 09:57:51 -0400

    Fix floating point errors.

--- app/models/billing/job_decorator.rb
+++ app/models/billing/job_decorator.rb
@@ -1,9 +1,17 @@
 module Billing
   class JobDecorator < ApplicationDecorator
-    CHARGE_RATE = 0.001
+    CHARGE_RATE = 0.1
 
     def charge
-      object.duration * CHARGE_RATE
+      return unless charge_cents.present?
+
+      charge_cents / 100.0
+    end
+
+    def charge_cents
+      return unless duration.present?
+
+      (object.duration * CHARGE_RATE).round
     end
   end
 end
