63ed30c5d09615e917512055a4d9235dc820b740
Jason Swett <jason@benfranklinlabs.com>
2024-10-23 17:55:13 -0400

    Squashed commit of the following:
    commit 9cd580dc1e6d8e5d6b7af3daa0c05db84fc8cf9c
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Oct 23 17:52:58 2024 -0400
    
        Fix JobEvent enum.
    
    commit 7ba35460730ad8daabba9dd521ab3a7475b853e6
    Author: Jason Swett <jason@benfranklinlabs.com>
    Date:   Wed Oct 23 17:49:38 2024 -0400
    
        Upgrade to Rails 7.2.

--- app/models/job_event.rb
+++ app/models/job_event.rb
@@ -2,16 +2,16 @@ class JobEvent < ApplicationRecord
   self.inheritance_column = :_type_not_used
   belongs_to :job, touch: true
 
-  enum type: {
-    job_machine_requested: 1,
-    job_machine_ready: 0,
-    repository_cloned: 2,
-    job_started: 3,
-    pre_script_started: 5,
-    pre_script_finished: 6,
-    test_suite_started: 9,
-    job_finished: 4,
-    image_build_started: 7,
-    image_build_finished: 8
-  }
+  enum :type, [
+    :job_machine_ready,
+    :job_machine_requested,
+    :repository_cloned,
+    :job_started,
+    :pre_script_started,
+    :pre_script_finished,
+    :test_suite_started,
+    :job_finished,
+    :image_build_started,
+    :image_build_finished
+  ]
 end
