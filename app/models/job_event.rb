c6b0e0e41ee225f4388a55aff9f7824a2fe460ac
Jason Swett <jason@benfranklinlabs.com>
2024-10-23 21:22:09 -0400

    Reorder JobEvent events.

--- app/models/job_event.rb
+++ app/models/job_event.rb
@@ -3,15 +3,15 @@ class JobEvent < ApplicationRecord
   belongs_to :job, touch: true
 
   enum :type, [
-    :job_machine_ready,
+    :image_build_started,
+    :image_build_finished,
     :job_machine_requested,
-    :repository_cloned,
+    :job_machine_ready,
     :job_started,
+    :job_finished,
     :pre_script_started,
     :pre_script_finished,
     :test_suite_started,
-    :job_finished,
-    :image_build_started,
-    :image_build_finished
+    :repository_cloned,
   ]
 end
